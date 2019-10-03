2e550f5ff0632711eb4de23fd2d8c64b831670e3
kapil jhajhria <jhajhria44@gmail.com>
2019-10-03 09:48:50 +0530

    two dice in center.
    without using expanded and flex

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Roll the Dice'),
-          backgroundColor: Colors.red,
-        ),
-        body: Container(),
-      ),
-    ),
+    MaterialApp(home: DicePage()),
   );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text('Roll the Dice'),
+        backgroundColor: Colors.red,
+      ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Image(
+              width: 180,
+              image: AssetImage('assets/dice1.png'),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Image(
+              width: 180,
+              image: AssetImage('assets/dice3.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
